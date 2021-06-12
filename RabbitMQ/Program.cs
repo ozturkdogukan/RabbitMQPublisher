28fcfd04f99366181f01ca282b701cd636121e91
Dogukan <reatline@gmail.com>
2021-06-12 16:13:02 +0300

    Add project files.

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RabbitMQSample
+{
+    class Program
+    {
+        private static readonly string _queueName = "DOGUKANOZTURK";
+        private static Publisher _publisher;
+
+
+        static void Main(string[] args)
+        {
+            string message = "deneme";
+            do
+            {
+                Console.WriteLine("İletmek istediğiniz mesajı giriniz");
+                message = Console.ReadLine();
+                _publisher = new Publisher(_queueName, message);
+            } while (!(message.Equals("q")));
+            Console.WriteLine("Bay bay");
+        }
+    }
+}
